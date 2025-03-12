a4213e9f36cf7d083ed87dd413788dca52a40936
George Olwal <olwalgeorge@live.com>
2025-03-12 10:42:50 +0300

    added deployed url to  route.rest file

--- w01-project/app/helpers/swagger.js
+++ w01-project/app/helpers/swagger.js
@@ -12,7 +12,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000/api',
+        url: 'https://cse341-w01-project-f4cy.onrender.com',
       },
     ],
     components: {
